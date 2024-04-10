22a7a3ef69b5a9f35ed930a443d48ccd881f1aec
Nicholas Sixbury <Nicholas.Sixbury@usda.gov>
2024-04-10 10:40:15 -0500

    Added method to get file from gui.

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,4 @@
-use fltk::{app::{self, App, Receiver, Sender}, button::{self, Button}, frame::Frame, prelude::{DisplayExt, GroupExt, WidgetBase, WidgetExt}, text, window::{self, Window}};
+use fltk::{app::{self, App, Receiver, Sender}, button::{self, Button}, dialog, frame::Frame, prelude::{DisplayExt, GroupExt, WidgetBase, WidgetExt}, text, window::{self, Window}};
 
 #[allow(dead_code)]
 pub struct GUI {
@@ -20,38 +20,27 @@ impl GUI {
     /// various widgets and UI settings.
     pub fn initialize() -> GUI {
         let c_grain_app = app::App::default();
-        let mut main_window = window::Window::default().with_size(900, 480).with_label("USDA C-Grain Summarizer");
+        let mut main_window = window::Window::default().with_size(600, 300).with_label("USDA C-Grain Summarizer");
+
+        let (s,r) = app::channel();
 
         // set up header information
         let mut header_buf = text::TextBuffer::default();
-        let mut header_box = text::TextDisplay::default().with_pos(10, 10).with_size(880, 140);
+        let mut header_box = text::TextDisplay::default().with_pos(10, 10).with_size(580, 140);
         header_box.set_buffer(header_buf.clone());
 
         header_buf.append("C-Grain Summarizer v##.##\n");
         header_buf.append("USDA-ARS Manhattan, KS\n");
 
-        let mut thresh_prompt_buf = text::TextBuffer::default();
-        let mut prompt_threshold = text::TextDisplay::new(10,260,300,40,"");
-        prompt_threshold.set_buffer(thresh_prompt_buf.clone());
-        thresh_prompt_buf.set_text("Please enter threshold. Default: 1.5");
-
-        let mut thresh_input_buf = text::TextBuffer::default();
-        let mut txt_thresh_input = text::TextEditor::new(320, 260, 570, 40, "");
-        txt_thresh_input.set_buffer(thresh_input_buf.clone());
-        thresh_input_buf.append("1.5");
-
-        // set up additional options
-        let btn_skip_lines = button::CheckButton::new(10, 310, 400, 40, "Skip a number of lines at start of Input File");
-        let mut skip_line_buf = text::TextBuffer::default();
-        let mut txt_skip_line_num = text::TextEditor::new(410,310,480,40,"");
-        txt_skip_line_num.set_buffer(skip_line_buf.clone());
-        skip_line_buf.append("4");
-        btn_skip_lines.set_checked(true);
+        // get input file from user
+        let mut input_file_btn = Button::default()
+            .with_label("Select Input CSV")
+            .with_pos(header_box.x(), header_box.y() + header_box.h() + 10)
+            .with_size(125, 25);
+        input_file_btn.emit(s.clone(), "GetFile".to_string());
 
         main_window.end();
         main_window.show();
-
-        let (s,r) = app::channel();
 
         GUI {
             app: c_grain_app,
@@ -71,4 +60,11 @@ impl GUI {
     pub fn wait(&self) -> bool {
         self.app.wait()
     }//end wait(&self)
+
+    /// Gets a file from the user
+    pub fn get_file() -> std::path::PathBuf {
+        let mut dialog = dialog::NativeFileChooser::new(dialog::NativeFileChooserType::BrowseFile);
+        dialog.show();
+        dialog.filename()
+    }//end get_file()
 }//end impl for GUI
